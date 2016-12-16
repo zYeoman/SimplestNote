1b72fefbf8786d7101b514202457cc92e9a72e9c
Yongwen Zhuang <zyeoman@163.com>
2016-12-16 22:03:43 +0800

    :fire: Delete useless line

--- app/src/main/java/com/yeoman/simplestnote/SimplestNoteDbHelper.java
+++ app/src/main/java/com/yeoman/simplestnote/SimplestNoteDbHelper.java
@@ -8,10 +8,6 @@ public class SimplestNoteDbHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "SimplestNote.db";
-    ;
-
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
 
     public SimplestNoteDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
